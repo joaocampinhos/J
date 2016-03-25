d54f6c84065f2db6b37fec9018040541de8671c6
joaocampinhos <joao.campinhos@gmail.com>
2016-03-25 16:31:32 +0000

    Composition support

---
+++ ASTMultiple.java
@@ -0,0 +1,27 @@
+public class ASTMultiple implements ASTNode {
+  ASTNode left, right;
+
+  public IValue eval(Env e) throws TypeError, Env.IdentifierDeclaredTwice, Env.UndeclaredIdentifier{
+
+    IValue v1 = left.eval(e);
+
+    e.beginScope();
+    IValue v2 = right.eval(e);
+    e.endScope();
+
+    return v2;
+  }
+
+  public ASTMultiple(ASTNode l, ASTNode r) {
+    left = l;
+    right = r;
+  }
+
+  public IType typeCheck(TypeEnv e) throws TypeError{
+
+    left.typeCheck(e);
+    IType res = right.typeCheck(e);
+
+    return res;
+  }
+}
